6e2160ed53b4dd1a1fd1a9382b0206fac5b23e3d
Falconoff <sokolovserega1982@gmail.com>
2022-02-01 22:51:26 +0200

    add new proj

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#888',
+
+        fontSize: 40,
+        textTransform: 'uppercase',
+        fontWeight: 700,
+        color: '#010101',
+      }}
+    >
+      React homework 04
+    </div>
+  );
+};
